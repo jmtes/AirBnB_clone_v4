93954a31d72ccfab12f4f3302ead6a54b23e8df6
GeorgeSolorio <George_solorio@yahoo.com>
2019-09-28 20:33:41 +0000

    Fixed typo

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -25,11 +25,11 @@ $(function() {
 		}
 	});
 	const $apiStatus = $.get('http://0.0.0.0:5001/api/v1/status/');
-	$apitStatus.done(function(data){
-  	if (data.status === 'OK') {
-    	$('DIV#api_status').addClass('available');
-  	} else {
-    	$('DIV#api_status').removeClass('available');
+	$apiStatus.done(function(data){
+  	  if (data.status === 'OK') {
+    	    $('DIV#api_status').addClass('available');
+  	  } else {
+    	    $('DIV#api_status').removeClass('available');
   	}
-});
+  });
 });
