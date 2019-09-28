61d3651c0692514800b10f236e902c9aec457e1d
GeorgeSolorio <George_solorio@yahoo.com>
2019-09-28 22:00:03 +0000

    task 4 stuff

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,40 @@
+$(function() {
+	let amenities = [];
+	$('input:checkbox').change(
+	function(){
+		if ($(this).is(':checked')) {
+			amenities.push($(this).data('name'));
+		} else {
+			let index = amenities.indexOf($(this).data('name'));
+			if (index > -1) {
+				amenities.splice(index, 1);
+			}
+		}
+		if (amenities.length === 0) {
+			$('div.amenities h4').html('&nbsp;');
+		} else {
+			let amenityString = '';
+			amenities.forEach(function(item, index) {
+				if (index !== 0) {
+					amenityString += ', ';
+				}
+				amenityString += item;
+			});
+			$('div.amenities h4').text(amenityString);
+		}
+	});
+	const $apiStatus = $.get('http://0.0.0.0:5001/api/v1/status/');
+	$apiStatus.done(function(data){
+		console.log(data);
+  	  if (data.status === 'OK') {
+		  console.log('im ok');
+			$('div#api_status').addClass('available');
+  	  } else {
+			$('div#api_status').removeClass('available');
+  	  }
+  	});
+        const $getPlace= $.post('http://0.0.0.0:5001/api/v1/places_search/');
+	$getPlace.done(funtion(data){
+		console.log(data);
+	});
+});
